4a0c70a90136dc02e3c7efe737a3de9251471a1b
Ray Pan <Ev3rlasting@users.noreply.github.com>
2025-01-11 22:02:34 +0800

    clippy and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use bip39::Mnemonic;
-use shamir_secret_sharing::ShamirSecretSharing as SSS;
 use num_bigint::BigInt;
 use rand::{thread_rng, RngCore};
-use std::{fmt::Display, io::{self, Write}};
+use shamir_secret_sharing::ShamirSecretSharing as SSS;
+use std::{
+    fmt::Display,
+    io::{self, Write},
+};
 
 const PRIME_HEX: &str = "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f";
 #[derive(Clone)]
@@ -11,10 +14,14 @@ struct Shamir39Share {
     share: Mnemonic,
     language: bip39::Language,
 }
-    
+
 impl Shamir39Share {
     fn new(index: usize, share: Mnemonic, language: bip39::Language) -> Self {
-        Self { index, share, language }
+        Self {
+            index,
+            share,
+            language,
+        }
     }
 }
 
@@ -33,7 +40,7 @@ impl Display for Shamir39Share {
             bip39::Language::Spanish => bip39::Language::Spanish.word_list(),
         };
 
-        let index_word = word_list[self.index as usize];
+        let index_word = word_list[self.index];
 
         write!(f, "{} {}", index_word, self.share)
     }
@@ -48,7 +55,12 @@ fn bytes_to_mnemonic(bytes: &[u8], language: bip39::Language) -> Mnemonic {
     Mnemonic::from_entropy_in(language, bytes).expect("Valid entropy")
 }
 
-fn generate_shares(mnemonic: &Mnemonic, threshold: usize, share_amount: usize, language: bip39::Language) -> Vec<Shamir39Share> {
+fn generate_shares(
+    mnemonic: &Mnemonic,
+    threshold: usize,
+    share_amount: usize,
+    language: bip39::Language,
+) -> Vec<Shamir39Share> {
     let secret_bytes = mnemonic.to_entropy();
     let secret = BigInt::from_bytes_be(num_bigint::Sign::Plus, &secret_bytes);
 
@@ -60,23 +72,36 @@ fn generate_shares(mnemonic: &Mnemonic, threshold: usize, share_amount: usize, l
 
     let shares = sss.split(secret);
 
-    shares.into_iter().map(|share| {
-        let share_bytes = share.1.to_bytes_be().1;
-        Shamir39Share::new(share.0, bytes_to_mnemonic(&share_bytes, language), language)
-    }).collect()
+    shares
+        .into_iter()
+        .map(|share| {
+            let share_bytes = share.1.to_bytes_be().1;
+            Shamir39Share::new(share.0, bytes_to_mnemonic(&share_bytes, language), language)
+        })
+        .collect()
 }
 
-fn reconstruct_secret(shares: &[Shamir39Share], threshold: usize, language: bip39::Language) -> Mnemonic {
+fn reconstruct_secret(
+    shares: &[Shamir39Share],
+    threshold: usize,
+    language: bip39::Language,
+) -> Mnemonic {
     let sss = SSS {
         threshold,
         share_amount: shares.len(),
         prime: BigInt::parse_bytes(PRIME_HEX.as_bytes(), 16).expect("Valid prime"),
     };
 
-    let bigint_shares = shares.iter().map(|share| {
-        let bytes = share.share.to_entropy();
-        (share.index, BigInt::from_bytes_be(num_bigint::Sign::Plus, &bytes))
-    }).collect::<Vec<(usize, BigInt)>>();
+    let bigint_shares = shares
+        .iter()
+        .map(|share| {
+            let bytes = share.share.to_entropy();
+            (
+                share.index,
+                BigInt::from_bytes_be(num_bigint::Sign::Plus, &bytes),
+            )
+        })
+        .collect::<Vec<(usize, BigInt)>>();
 
     let reconstructed_secret = sss.recover(&bigint_shares);
     let secret_bytes = reconstructed_secret.to_bytes_be().1;
@@ -131,13 +156,17 @@ fn main() {
             print!("Enter the threshold (n) for SSS (default is 3): ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut threshold_input = String::new();
-            io::stdin().read_line(&mut threshold_input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut threshold_input)
+                .expect("Failed to read line");
             let threshold: usize = threshold_input.trim().parse().unwrap_or(3);
 
             print!("Enter the total number of shares (m) for SSS (default is 5): ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut share_amount_input = String::new();
-            io::stdin().read_line(&mut share_amount_input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut share_amount_input)
+                .expect("Failed to read line");
             let share_amount: usize = share_amount_input.trim().parse().unwrap_or(5);
 
             let entropy = generate_secure_entropy(32);
@@ -158,19 +187,25 @@ fn main() {
             print!("Enter your existing mnemonic (split by space): ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut existing_mnemonic = String::new();
-            io::stdin().read_line(&mut existing_mnemonic).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut existing_mnemonic)
+                .expect("Failed to read line");
             let mnemonic = Mnemonic::parse(existing_mnemonic.trim()).expect("valid mnemonic");
 
             print!("Enter the threshold (n) for SSS (default is 3): ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut threshold_input = String::new();
-            io::stdin().read_line(&mut threshold_input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut threshold_input)
+                .expect("Failed to read line");
             let threshold: usize = threshold_input.trim().parse().unwrap_or(3);
 
             print!("Enter the total number of shares (m) for SSS (default is 5): ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut share_amount_input = String::new();
-            io::stdin().read_line(&mut share_amount_input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut share_amount_input)
+                .expect("Failed to read line");
             let share_amount: usize = share_amount_input.trim().parse().unwrap_or(5);
 
             let shares = generate_shares(&mnemonic, threshold, share_amount, language);
@@ -186,7 +221,9 @@ fn main() {
             print!("Enter the number of shares you will provide: ");
             io::stdout().flush().expect("Failed to flush stdout");
             let mut num_shares_input = String::new();
-            io::stdin().read_line(&mut num_shares_input).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut num_shares_input)
+                .expect("Failed to read line");
             let num_shares: usize = num_shares_input.trim().parse().unwrap_or(3);
 
             let mut shares = Vec::new();
@@ -194,14 +231,19 @@ fn main() {
                 print!("Enter share {}: ", i + 1);
                 io::stdout().flush().expect("Failed to flush stdout");
                 let mut share_input = String::new();
-                io::stdin().read_line(&mut share_input).expect("Failed to read line");
-                
-                let words: Vec<&str> = share_input.trim().split_whitespace().collect();
+                io::stdin()
+                    .read_line(&mut share_input)
+                    .expect("Failed to read line");
+
+                let words: Vec<&str> = share_input.split_whitespace().collect();
                 let indexed_word = words[0];
-                let index = language.word_list().iter().position(|&w| w == indexed_word)
+                let index = language
+                    .word_list()
+                    .iter()
+                    .position(|&w| w == indexed_word)
                     .expect("Invalid share. Only supports shares generated by this tool");
-                
-                let share = Mnemonic::parse(&words[1..].join(" ")).expect("valid mnemonic");
+
+                let share = Mnemonic::parse(words[1..].join(" ")).expect("valid mnemonic");
                 shares.push(Shamir39Share::new(index, share, language));
             }
 
@@ -232,14 +274,20 @@ mod tests {
         assert_eq!(shares.len(), share_amount);
         let mut indices: Vec<usize> = (0..share_amount).collect();
         indices.shuffle(&mut thread_rng());
-        let selected_shares: Vec<Shamir39Share> = indices.iter().map(|&i| shares[i].clone()).collect();
-        let reconstructed_mnemonic = reconstruct_secret(&selected_shares[0..threshold], threshold, language);
-        assert_eq!(existing_mnemonic, reconstructed_mnemonic, "Reconstruction failed");
+        let selected_shares: Vec<Shamir39Share> =
+            indices.iter().map(|&i| shares[i].clone()).collect();
+        let reconstructed_mnemonic =
+            reconstruct_secret(&selected_shares[0..threshold], threshold, language);
+        assert_eq!(
+            existing_mnemonic, reconstructed_mnemonic,
+            "Reconstruction failed"
+        );
     }
 
     #[test]
     fn test_random_shares_reconstruction_chinese() {
-        let existing_mnemonic_str = "密 勇 诉 灌 高 日 瓦 从 纠 纹 方 喝 言 七 推 愿 患 黎 浇 又 五 仓 甘 线";
+        let existing_mnemonic_str =
+            "密 勇 诉 灌 高 日 瓦 从 纠 纹 方 喝 言 七 推 愿 患 黎 浇 又 五 仓 甘 线";
         let existing_mnemonic = Mnemonic::parse(existing_mnemonic_str).expect("valid mnemonic");
 
         let threshold = 6;
@@ -250,10 +298,13 @@ mod tests {
 
         let mut indices: Vec<usize> = (0..share_amount).collect();
         indices.shuffle(&mut thread_rng());
-        let selected_shares: Vec<Shamir39Share> = indices.iter().map(|&i| shares[i].clone()).collect();
-        let reconstructed_mnemonic = reconstruct_secret(&selected_shares[0..threshold], threshold, language);
-        assert_eq!(existing_mnemonic, reconstructed_mnemonic, "Reconstruction failed with random shares");
+        let selected_shares: Vec<Shamir39Share> =
+            indices.iter().map(|&i| shares[i].clone()).collect();
+        let reconstructed_mnemonic =
+            reconstruct_secret(&selected_shares[0..threshold], threshold, language);
+        assert_eq!(
+            existing_mnemonic, reconstructed_mnemonic,
+            "Reconstruction failed with random shares"
+        );
     }
-
-
 }
